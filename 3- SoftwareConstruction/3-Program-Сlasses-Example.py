4236df6c19c1c565ffbf1e8709b5199911e70b84
ruslan-tsytkov <79439984+ruslan-tsytkov@users.noreply.github.com>
2023-05-10 23:01:42 +0300

    Create 3-Program-Сlasses-Example.py

---
+++ 3- SoftwareConstruction/3-Program-Сlasses-Example.py
@@ -0,0 +1,25 @@
+class User_sportsman:
+    def init(self, user_id, name, phone):
+        self.user_id = user_id
+        self.name = user_name
+        self.phone = phone
+
+    def chat():
+        return show_messages()
+
+    def sign_up_comp():
+        return show_competitions()
+
+class Competition:
+    def init(self):
+        competition_id = new_competition_id()
+        competition_name = input("Введіть назву змагань")
+        competition_date = input("Введіть дату змагань")
+        competition_place = input("Введіть місце змагань")
+        sports = input("Введіть вид спорту")
+        number_of_participants = input("Введіть кількість учасників")
+
+    def create_ticket(self):
+        ticket_id = new_message_id()
+        self.competition_id = competition_id
+        user_id = input("Введіть id учасника змагань")
